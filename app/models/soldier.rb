807484eae8dd836872018000bddfb44b5137e5e3
Dale Campbell <oshuma@gmail.com>
2008-04-19 03:02:31 -0500

    Minor update.

--- app/models/soldier.rb
+++ app/models/soldier.rb
@@ -10,5 +10,6 @@ class Soldier < ActiveRecord::Base
 
   validates_presence_of :army_id, :squad_id
   validates_presence_of :name, :message => 'must not be empty.'
+  validates_uniqueness_of :name, :message => 'is already taken.'
   validates_presence_of :health, :armor, :actions, :accuracy, :strength
 end
